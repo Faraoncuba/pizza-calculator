b55359b3d75b2f83a859af5edafbc193a1324407
Faraoncuba <ramsesglez@gmail.com>
2025-04-22 09:25:00 +0200

    Add files via upload

---
+++ src/components/ui/radio-group.tsx
@@ -0,0 +1,6 @@
+// src/components/ui/radio-group.tsx
+import * as React from "react";
+import { RadioGroup as RadixRadioGroup, RadioGroupItem as RadixRadioGroupItem } from "@radix-ui/react-radio-group";
+
+export const RadioGroup = RadixRadioGroup;
+export const RadioGroupItem = RadixRadioGroupItem;
